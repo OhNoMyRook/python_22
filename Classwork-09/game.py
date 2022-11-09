205866ae2773182cba1b2aded44df292288f4738
OhNoMyRook <kulikov.mo@phystech.edu>
2022-11-09 18:37:24 +0300

    add SOME BALLS

--- Classwork-09/game.py
+++ Classwork-09/game.py
@@ -5,9 +5,14 @@ from random import randint
 pygame.init()
 pygame.font.init()
 
+WIDTH = 1200
+HEIGTH = 900
+r_min = 40
+r_max = 100
 FPS = 2
-screen = pygame.display.set_mode((1200, 900))
+screen = pygame.display.set_mode((WIDTH, HEIGTH))
 pygame.display.set_caption("Catch the ball")
+clock = pygame.time.Clock()
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -19,22 +24,42 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-def new_ball():
-    global x, y, r
-    x = randint(100,1000)
-    y = randint(100,800)
-    r = randint(30,60)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
-
 pygame.display.update()
-clock = pygame.time.Clock()
 start_time = pygame.time.get_ticks()  
 stop_after = 20 * 1000 
 
 s = 0
 
 font_name = pygame.font.match_font('arial')
+
+def new_ball_1():
+    global x, y, r
+    x = randint(r_max,WIDTH - r_max)
+    y = randint(r_max,HEIGTH - r_max)
+    r = randint(r_min,r_max)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x, y), r)
+
+def new_ball_2():
+    global x_1, y_1, r_1
+    x_1 = randint(r_max,WIDTH - r_max)
+    y_1 = randint(r_max,HEIGTH - r_max)
+    r_1 = randint(r_min,r_max)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x_1, y_1), r_1)
+
+def new_ball_3():
+    global x_2, y_2, r_2
+    x_2 = randint(r_max,WIDTH - r_max)
+    y_2 = randint(r_max,HEIGTH - r_max)
+    r_2 = randint(r_min,r_max)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x_2, y_2), r_2)
+
+def some_balls():
+    new_ball_1()
+    new_ball_2()
+    new_ball_3()
 
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
@@ -51,7 +76,7 @@ def show_go_screen():
     screen.fill(BLUE)
     draw_text(screen, "Количество очков: ", 40, 590, 400)
     draw_text(screen, str(s), 40, 750, 400)
-    draw_text(screen, "Press a key to play", 30, 600, 600)
+    draw_text(screen, "Press any key to play", 30, 600, 600)
     pygame.display.flip()
     waiting = True
     while waiting:
@@ -65,6 +90,7 @@ def show_go_screen():
                 screen.fill(BLACK)
                     
 running = True
+game_over = True
 
 while running:
     clock.tick(FPS)
@@ -73,13 +99,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if (math.sqrt(((event.pos[0])-x)**2 + ((event.pos[1])-y)**2)) <= r:
+            if (math.sqrt(((event.pos[0])-x)**2 + ((event.pos[1])-y)**2)) <= r or (math.sqrt(((event.pos[0])-x_1)**2 + ((event.pos[1])-y_1)**2)) <= r_1 or (math.sqrt(((event.pos[0])-x_2)**2 + ((event.pos[1])-y_2)**2)) <= r_2:
                 s+=1
     points()    
     if (current_time - start_time) >= stop_after:
+        game_over = True
+    if game_over:    
         show_go_screen()
+        game_over = False
         s = 0
         start_time = current_time 
-    new_ball()
+    some_balls()
     pygame.display.update()
     screen.fill(BLACK)
